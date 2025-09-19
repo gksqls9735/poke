19c03e3f5bf9146b20d91e3d8c775066dcfc6803
서한빈 <gksqls9735@gmail.com>
2025-09-19 11:39:59 +0900

    Feat: Implement locale-based language detection on initial page load

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(HttpApi) // 번역 파일을 서버에서 불러오기 위한 백엔드 플러그인
+  .use(LanguageDetector)  // 언어 감지 플러그인
   .use(initReactI18next) // i18next를 react-i18next에 연결
   .init({
-    // 기본 언어 설정
-    lng: 'ko',
     //lng 키에 해당하는 언어가 없을 경우 fallback 언어 사용
     fallbackLng: 'en',
     // 디버깅: 개발 중 콘솔에 로그 출력
